3029af4c1a66468de7d646152844024175c58fc7
Lin Li <729373074@qq.com>
2018-04-23 14:50:14 -0400

    klkjlk

---
+++ Checkers/Piece.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Checkers
+//
+//  Created by Lin Li on 4/23/18.
+//  Copyright © 2018 Lin Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Piece : UIButton {
+    
+}
